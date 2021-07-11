00f305fa7108a5d17a9d4c3edc1cf69da521100b
AlexSashin <pyqan@outlook.com>
2021-07-11 14:29:20 +0500

    add reload add bind

--- app/restart.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/sh
-watchmedo shell-command --patterns='*.py;*.html;*.css;*.js' --recursive --command='echo "${watch_src_path}" && kill -HUP `cat gunicorn.pid` & gunicorn --reload --workers=2 --bind=0.0.0.0:8000 mysite.wsgi:application'
